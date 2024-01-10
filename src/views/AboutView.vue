b8d5969bb731d93960517cdaa168c64b4cccc46b
codeFabFit <fabgar14@gmail.com>
2024-01-10 15:53:38 -0500

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>About The Company</h1>
+    <img src="https://placehold.co/600x400" class="company">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ut est sequi dignissimos, ea iure aliquid provident adipisci impedit voluptatibus quia quidem ipsum incidunt dolores error in reiciendis, modi eos.</p>
+    <h2>The Mission</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iusto ex, atque id rerum cumque alias fugiat, incidunt aspernatur, excepturi nobis nostrum unde rem consequatur. Aut perspiciatis sint a repellat.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iusto ex, atque id rerum cumque alias fugiat, incidunt aspernatur, excepturi nobis nostrum unde rem consequatur. Aut perspiciatis sint a repellat.</p>
+    <h2>The Employees</h2>
+    <img src="https://placehold.co/150x200" class="employees">
+    <img src="https://placehold.co/150x200" class="employees">
+    <img src="https://placehold.co/150x200" class="employees">
+    
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid obcaecati mollitia dolor doloribus amet, neque at error impedit eos reiciendis natus a sed molestias tenetur libero qui, pariatur sunt!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos esse iure cum. Est dolor, quasi dolore obcaecati accusamus itaque saepe, distinctio, laborum libero optio odit magni recusandae tempora consectetur numquam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis laudantium, assumenda temporibus natus ullam cumque officia iusto reprehenderit tempore id officiis ex inventore, nulla voluptate eaque aperiam aspernatur alias tempora.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis laborum non recusandae, dolores possimus ipsam ea, esse explicabo asperiores omnis sit vel. Ab voluptatum ipsum quasi. Vero nulla accusamus et?</p>
+  
+    
+  </div>
+</template>
+
+<style>
+.company {
+  padding: 10px;
+  margin: 10px;
+}
+.employees {
+  padding: 10px;
+  margin: 10px;
+
+}
+</style>
