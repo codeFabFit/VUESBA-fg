f794691822650b6d207b369fb87b3df9eb3f1fa6
codeFabFit <fabgar14@gmail.com>
2024-01-10 16:20:59 -0500

    added contact page

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="https://media.giphy.com/media/l41lJ8ywG1ncm9FXW/giphy.gif" width="225" height="175" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Brain Wonders 🧠" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+
       </nav>
     </div>
   </header>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="contact">
+      <h1>About The Company</h1>
+     
+      
+    </div>
+  </template>
